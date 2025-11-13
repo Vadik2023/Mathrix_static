46a6ce5e13a72cb8f8fa066ff1a64f834eae8448
Vadik2023 <133889858+Vadik2023@users.noreply.github.com>
2025-11-13 23:44:01 +0100

    Дике, але дороблене Меню

--- Mathrix_static/Static_mathrix_functions.c
+++ Mathrix_static/Static_mathrix_functions.c
@@ -2,8 +2,12 @@
 
 void Input(int matrix[N][M])
 {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
+    printf("Please, write matrix elements\n");
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
             scanf("%d", &matrix[i][j]);
         }
     }
@@ -24,15 +28,19 @@ void Menu(int matrix[N][M])
 
         int func_num;
 
+        // проверка корректного ввода числа
         if (scanf("%d", &func_num) != 1)
         {
             printf("Invalid input. Please enter a number.\n\n");
-            while (getchar() != '\n');
+            while (getchar() != '\n'); // очистка буфера
             continue;
         }
 
-        while (getchar() != '\n');
+        // очищаем остатки строки после числа (Enter или пробелы)
+        int tmp;
+        while ((tmp = getchar()) != '\n' && tmp != EOF);
 
+        // выбор функции
         switch (func_num)
         {
         case 1:
@@ -48,16 +56,25 @@ void Menu(int matrix[N][M])
             printf("Invalid function number.\n");
         }
 
+        // безопасный цикл для вопроса y/n
         while (1)
         {
             printf("\nWould you like to continue? (y/n): ");
-            choice = (char)getchar();
 
-            while (getchar() != '\n');
+            int c;
 
-            if (choice == 'y' || choice == 'Y')
+            // пропускаем все пробелы и Enter от предыдущего ввода
+            do {
+                c = getchar();
+                if (c == EOF) return; // защита
+            } while (c == ' ' || c == '\n');
+
+            // очищаем остаток строки после ввода
+            while ((tmp = getchar()) != '\n' && tmp != EOF);
+
+            if (c == 'y' || c == 'Y')
                 break;
-            else if (choice == 'n' || choice == 'N')
+            else if (c == 'n' || c == 'N')
             {
                 printf("\nreturn to main()\n");
                 return;
@@ -68,10 +85,15 @@ void Menu(int matrix[N][M])
     }
 }
 
+
 void Print(int matrix[N][M])
 {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
+    printf("Your matrix:\n");
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
             printf("%d ", matrix[i][j]);
         }
         printf("\n");
